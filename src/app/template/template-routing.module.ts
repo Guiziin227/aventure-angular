1da1c545adbd8cb5788c27121f765ccfa8c8fd5b
Guiziin227 <gh63388@gmail.com>
2025-06-03 13:05:52 -0300

    refactor template module and routing: add categorias module and update layout structure

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LayoutComponent} from './layout/layout.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path: 'paginas',
     component: LayoutComponent,
-  }
+    children: [
+      {
+        path: 'categorias',
+        loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule)
+      }
+    ]
+    }
 ];
 
 @NgModule({
