a219b829db7943ca5f0e1174ad365ecda9bb50b4
Guiziin227 <gh63388@gmail.com>
2025-06-07 16:26:13 -0300

    refactor LugarComponent to implement OnInit and load categories on initialization

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Categoria } from "../../categorias/categoria";
+import { CategoriaService } from "../../categorias/categoria.service";
 
 @Component({
   selector: "app-lugar",
@@ -8,17 +9,29 @@ import { Categoria } from "../../categorias/categoria";
   templateUrl: "./lugar.component.html",
   styleUrl: "./lugar.component.scss",
 })
-export class LugarComponent {
+export class LugarComponent implements OnInit {
   camposForm: FormGroup;
   categorias: Categoria[] = [];
 
-  constructor() {
+  constructor(private categoriaService: CategoriaService) {
     this.camposForm = new FormGroup({
       nome: new FormControl("", Validators.required),
       categoria: new FormControl("", Validators.required),
       localizacao: new FormControl("", Validators.required),
       urlImagem: new FormControl("", Validators.required),
       avaliacao: new FormControl("", Validators.required),
+    });
+  }
+
+  ngOnInit() {
+    //puxamos as categorias do serviço ao inicializar o componente
+    this.categoriaService.getAll().subscribe({
+      next: (categorias) => {
+        this.categorias = categorias;
+      },
+      error: (err) => {
+        console.error("Erro ao carregar categorias: ", err);
+      },
     });
   }
 
