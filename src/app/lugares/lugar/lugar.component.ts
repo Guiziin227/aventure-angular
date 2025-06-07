9e7c5f0b67d6a8a02f8a1b799affe556a4784222
Guiziin227 <gh63388@gmail.com>
2025-06-07 16:21:19 -0300

    refactor LugarComponent for consistent formatting and improved readability

--- src/app/lugares/lugar/lugar.component.ts
+++ src/app/lugares/lugar/lugar.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Categoria } from "../../categorias/categoria";
 
 @Component({
-  selector: 'app-lugar',
+  selector: "app-lugar",
   standalone: false,
-  templateUrl: './lugar.component.html',
-  styleUrl: './lugar.component.scss'
+  templateUrl: "./lugar.component.html",
+  styleUrl: "./lugar.component.scss",
 })
 export class LugarComponent {
+  camposForm: FormGroup;
+  categorias: Categoria[] = [];
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl("", Validators.required),
+      categoria: new FormControl("", Validators.required),
+      localizacao: new FormControl("", Validators.required),
+      urlImagem: new FormControl("", Validators.required),
+      avaliacao: new FormControl("", Validators.required),
+    });
+  }
+
+  save() {
+    console.log("Dados salvos: ", this.camposForm.value);
+  }
 }
