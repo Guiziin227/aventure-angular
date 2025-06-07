9f58df66a4a866ce7712de6c098ed33f9566adf1
Guiziin227 <gh63388@gmail.com>
2025-06-07 16:52:45 -0300

    refactor LugarService for improved structure and readability

--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Lugar } from "./lugar";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class LugarService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  save(lugar: Lugar): Observable<Lugar> {
+    return this.http.post<Lugar>("http://localhost:3000/lugares", lugar);
+  }
+
+  getAll(): Observable<Lugar[]> {
+    return this.http.get<Lugar[]>("http://localhost:3000/lugares");
+  }
 }
