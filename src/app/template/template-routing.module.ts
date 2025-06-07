d9955681a43a55e401715da79c233d68f4de4486
Guiziin227 <gh63388@gmail.com>
2025-06-07 16:20:56 -0300

    refactor TemplateRoutingModule for consistent formatting and improved readability

--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,22 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { LayoutComponent } from "./layout/layout.component";
 
 const routes: Routes = [
   {
-    path: 'paginas',
+    path: "paginas",
     component: LayoutComponent,
     children: [
       {
-        path: 'categorias',
-        loadChildren: () => import('../categorias/categorias.module').then(m => m.CategoriasModule)
-      }
-    ]
-    }
+        path: "categorias",
+        loadChildren: () =>
+          import("../categorias/categorias.module").then(
+            (m) => m.CategoriasModule
+          ),
+        pathMatch: "full",
+      },
+      {
+        path: "lugares",
+        loadChildren: () =>
+          import("../lugares/lugares.module").then((m) => m.LugaresModule),
+        pathMatch: "full",
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TemplateRoutingModule { }
+export class TemplateRoutingModule {}
