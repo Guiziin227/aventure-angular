cb52e832eeb03f61ec599c08186f498b5c0747e7
Guiziin227 <gh63388@gmail.com>
2025-06-09 13:11:11 -0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./template/template.module').then(m => m.TemplateModule)
-  }
+    path: "",
+    loadChildren: () =>
+      import("./template/template.module").then((m) => m.TemplateModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
