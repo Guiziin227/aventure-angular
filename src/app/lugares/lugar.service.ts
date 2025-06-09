b62c8f1c0b3b19eb946fb2ebbd2192f3e6ef6fa7
Guiziin227 <gh63388@gmail.com>
2025-06-09 12:56:30 -0300

    add filterByNomeOrCategoria method to LugarService for enhanced data retrieval

--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Lugar } from "./lugar";
@@ -15,5 +15,26 @@ export class LugarService {
 
   getAll(): Observable<Lugar[]> {
     return this.http.get<Lugar[]>("http://localhost:3000/lugares");
+  }
+
+  filterByNomeOrCategoria(
+    nome: string,
+    categoria: string
+  ): Observable<Lugar[]> {
+    let parametros = new HttpParams();
+
+    if (nome) {
+      parametros = parametros.set("nome_like", nome);
+    }
+
+    if (categoria) {
+      parametros = parametros.set("categoria", categoria);
+    }
+
+    console.log(parametros.toString());
+
+    return this.http.get<Lugar[]>("http://localhost:3000/lugares", {
+      params: parametros,
+    });
   }
 }
