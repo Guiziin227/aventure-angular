fabc9401810be86f8e3fd32002180309dab169f5
Guiziin227 <gh63388@gmail.com>
2025-06-16 14:14:33 -0300

    fix

--- src/app/template/layout/layout.component.ts
+++ src/app/template/layout/layout.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { filter, map } from "rxjs/operators";
+import { LayoutProps } from "./layoutprops";
 
 @Component({
-  selector: 'app-layout',
+  selector: "app-layout",
   standalone: false,
-  templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  templateUrl: "./layout.component.html",
+  styleUrl: "./layout.component.scss",
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  props: LayoutProps = { title: "", subtitle: "" };
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(() => this.activatedRoute.firstChild !== null),
+        map(() => this.obterPropriedades())
+      )
+      .subscribe((props: LayoutProps) => {
+        this.props = props;
+      });
+  }
+
+  obterPropriedades(): LayoutProps {
+    let childRoute = this.activatedRoute.firstChild;
+
+    while (childRoute?.firstChild) {
+      childRoute = childRoute.firstChild;
+    }
+
+    return childRoute?.snapshot.data as LayoutProps;
+  }
 }
--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -14,18 +14,30 @@ const routes: Routes = [
             (m) => m.CategoriasModule
           ),
         pathMatch: "full",
+        data: {
+          title: "Categorias",
+          subtitle: "Registre novas categorias",
+        },
       },
       {
         path: "lugares",
         loadChildren: () =>
           import("../lugares/lugares.module").then((m) => m.LugaresModule),
         pathMatch: "full",
+        data: {
+          title: "Lugares",
+          subtitle: "Registre novos lugares",
+        },
       },
       {
         path: "galeria",
         loadChildren: () =>
           import("../galeria/galeria.module").then((m) => m.GaleriaModule),
         pathMatch: "full",
+        data: {
+          title: "Galeria",
+          subtitle: "Veja os lugares cadastrados",
+        },
       },
     ],
   },
