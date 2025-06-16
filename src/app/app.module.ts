679aa45da6918d84c51db754bd3457f7ed94e926
Guiziin227 <gh63388@gmail.com>
2025-06-16 14:14:45 -0300

    feat: add LandingpageComponent to app module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { provideHttpClient, withFetch } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingpageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideHttpClient(withFetch())],
   bootstrap: [AppComponent],
