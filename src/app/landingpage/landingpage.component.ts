23029b7d6ea5957843fcd2edec7cca633b6e44cd
Guiziin227 <gh63388@gmail.com>
2025-06-16 14:27:28 -0300

    feat: enhance LandingpageComponent with profile management and navigation

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Profile} from './profile.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-landingpage',
@@ -8,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class LandingpageComponent {
 
+  profile: Profile | undefined;
+
+  constructor(private router: Router) {
+  }
+
+  navegation(){
+    this.router.navigate(['/paginas/galeria']);
+  }
+
+  logarComGoogle() {
+  }
+
+  isLoggedIn(): boolean{
+    return !!this.profile;
+  }
 }
